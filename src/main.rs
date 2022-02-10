19a13c4ca130212e4d46fa83e019950533c2e589
Gabriel <68819302+obsidianical@users.noreply.github.com>
2022-02-10 11:42:20 +0100

    better command parsing and the fucking cat died again!
    weeeeeeeeee

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use std::io;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	//let command: String = args.get(1).unwrap().clone();
-	match args.get(1).unwrap().as_str() {
-		"cat" => cat(args),
-		_ => println!("Command not found.")
-	}
-	//println!("{:?}", command);
+
+    let command = Command::parse_from(args);
+
+	//match args.get(1).unwrap().as_str() {
+		//"cat" => cat(args),
+		//_ => println!("Command not found.")
+	//}
 }
 
 fn cat(args: Vec<String>) {
@@ -34,9 +35,9 @@ fn cat(args: Vec<String>) {
 
 fn stdin_to_stdout_loop() -> ! {
 	let stdin = io::stdin();
-	let mut inpt: String = String::new();
 	
     loop {
+        let mut inpt: String = String::new();
         match stdin.read_line(&mut inpt) {
             Ok(v) => v,
             Err(e) => panic!("Error reading stdin: {:?}", e),
@@ -44,3 +45,58 @@ fn stdin_to_stdout_loop() -> ! {
         print!("{}", inpt);
     }
 }
+
+fn err_out(reason: String, exit_code: i32) -> ! {
+    println!("Error: '{}'", reason);
+    process::exit(exit_code);
+}
+
+#[derive(Debug)]
+struct CommandParam {
+    value: String,
+    is_flag: bool
+}
+
+#[derive(Debug)]
+struct Command {
+    name: String,
+    args: Vec<CommandParam>,
+    executable_path: String,
+}
+
+impl Command {
+    pub fn parse_from(arglist: Vec<String>) -> Command {
+        let mut command = Command {
+            name: match arglist.get(1) {
+                Some(v) => v.to_string(),
+                None => err_out(String::from("No command provided!"), 1),
+            },
+            args: vec![],
+            executable_path: arglist.get(0).unwrap().to_string(),
+        };
+
+        for arg in &arglist[2..arglist.len()] {
+            if arg.chars().nth(0).unwrap() == '-' {
+                if arg.chars().nth(1).unwrap() == '-' {
+                    command.args.push(CommandParam { 
+                        value: String::from(&arg[2..arg.len()]),
+                        is_flag: true
+                    });
+                } else {
+                    command.args.push(CommandParam { 
+                        value: String::from(&arg[1..arg.len()]),
+                        is_flag: true
+                    });
+                }
+            } else {
+                command.args.push(CommandParam {
+                    value: arg.to_string(),
+                    is_flag: false
+                });
+            }
+        }
+        println!("{:?}", command);
+        command
+    }
+}
+
