ac0f9d8b0ec8991737f02f00273b67b45515df09
Gabriel <68819302+obsidianical@users.noreply.github.com>
2022-02-11 13:52:12 +0100

    the cat can meow again

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,30 @@ fn main() {
 
     let command = Command::parse_from(args);
 
-	//match args.get(1).unwrap().as_str() {
-		//"cat" => cat(args),
-		//_ => println!("Command not found.")
-	//}
+    match command.name.as_str() {
+        "cat" => cat(command),
+        _ => println!("Command not found.")
+    }
 }
 
-fn cat(args: Vec<String>) {
-	let path = match args.get(2) {
-		Some(v) => v,
-		None => stdin_to_stdout_loop()
-	};
-
-    if path == "-" {
+fn cat(cmd: Command) {
+    let paths = if cmd.get_params().len() == 0 {
         stdin_to_stdout_loop();
+    } else {
+        cmd.get_params()
+    };
+
+    for path in paths {
+        if path.value.as_str() == "-" {
+            stdin_to_stdout_loop();
+        }
+
+        let file = match fs::read_to_string(path.value.as_str()) {
+            Ok(v) => v,
+            Err(e) => err_out(format!("Error while reading file: {}", e), 1)
+        };
+        print!("{}", file);
     }
-	
-	println!("{}", match fs::read_to_string(path) {
-		Ok(v) => v,
-		Err(v) => {
-			println!("Error when reading file: {}", v); 
-			process::exit(1);
-		},
-	});
 }
 
 fn stdin_to_stdout_loop() -> ! {
@@ -76,17 +77,19 @@ impl Command {
         };
 
         for arg in &arglist[2..arglist.len()] {
-            if arg.chars().nth(0).unwrap() == '-' {
+            if arg.chars().nth(0).unwrap() == '-' && arg.len() != 1 {
                 if arg.chars().nth(1).unwrap() == '-' {
                     command.args.push(CommandParam { 
                         value: String::from(&arg[2..arg.len()]),
                         is_flag: true
                     });
                 } else {
-                    command.args.push(CommandParam { 
-                        value: String::from(&arg[1..arg.len()]),
-                        is_flag: true
-                    });
+                    for i in 1..arg.len() {
+                        command.args.push(CommandParam {
+                            value: arg.chars().nth(i).unwrap().to_string(),
+                            is_flag: true
+                        })
+                    }
                 }
             } else {
                 command.args.push(CommandParam {
@@ -95,8 +98,27 @@ impl Command {
                 });
             }
         }
-        println!("{:?}", command);
+
         command
+    }
+
+    fn get_args(&self, get_flags: bool) -> Vec<&CommandParam> {
+        let mut res: Vec<&CommandParam> = vec![];
+
+        for arg in &self.args {
+            if arg.is_flag == get_flags {
+                res.push(arg);
+            }
+        }
+
+        res
+    }
+
+    pub fn get_flags(&self) -> Vec<&CommandParam> {
+        self.get_args(true)
+    }
+    pub fn get_params(&self) -> Vec<&CommandParam> {
+        self.get_args(false)
     }
 }
 
