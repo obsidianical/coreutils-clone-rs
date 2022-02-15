0ab4312a722cf3678183ebc57e5d615e8fec1242
Gabriel <68819302+obsidianical@users.noreply.github.com>
2022-02-15 15:52:32 +0100

    added line numbering

--- src/commands/cat.rs
+++ src/commands/cat.rs
@@ -17,10 +17,21 @@ pub fn cat(cmd: Command) {
             stdin_to_stdout_loop();
         }
 
-        let file = match fs::read_to_string(path.value.as_str()) {
+
+        let mut file = match fs::read_to_string(path.value.as_str()) {
             Ok(v) => v,
             Err(e) => err_out(format!("Error while reading file: {}", e), 1)
         };
+
+        for arg in &cmd.args {
+            if arg.arg_type == ArgType::Param{
+                continue
+            }
+
+            if arg.check_flag("n", "--number") {
+                file = number_lines(&file);
+            }
+        }
 
         print!("{}", file);
     }
@@ -39,4 +50,18 @@ fn stdin_to_stdout_loop() -> ! {
 
         print!("{}", inpt);
     }
+}
+
+fn number_lines(data: &str) -> String {
+    let lines = data.lines().enumerate()/*.collect::<Vec<&str>>()*/;
+    let mut r = String::new();
+    
+    for (i, line) in lines {
+        // by default 5 spaces before a number, reduce for every digit
+        let spaces_before = 5 - (i as f32).log10() as usize;
+        let line = format!("{}{}  {}\n", " ".repeat(spaces_before), i, line);
+        r.push_str(&line);
+    }
+
+    r
 }
